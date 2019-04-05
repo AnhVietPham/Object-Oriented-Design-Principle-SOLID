802a0ea3a5eafd0b5f33db28fb9d81d0437dd986
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-04-06 06:34:16 +0700

    SRP Principle: Add EmployeeDAO.java

---
+++ src/com/avp/srp/EmployeeDAO.java
@@ -0,0 +1,29 @@
+package com.avp.srp;
+
+public class EmployeeDAO {
+    private DatabaseConnectionManager connectionDBManager;
+
+    public EmployeeDAO(){
+        connectionDBManager = DatabaseConnectionManager.getInstance();
+    }
+
+    public void saveEmployee(Employee emp) {
+        /*
+        * This method will use to save employee to DB
+        * connectionDBManager.connect()
+        *    // To do something
+        * connectionDBManager.disconnect()
+        * */
+        System.out.println("Employee saved to DB");
+    }
+
+    public void deleteEmployee(Employee emp) {
+        /*
+        * This method will remove from DB
+        * connectionDBManager.connect()
+        *    // To do something
+        * connectionDBManager.disconnect()
+        * */
+        System.out.println("Employee removed from DB");
+    }
+}
