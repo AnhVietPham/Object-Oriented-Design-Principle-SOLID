e562721bc55cebc7acc92e1e58489f17e5d58f70
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-04-06 14:45:27 +0700

    OCP Principle: Add Nurse.java, Employee.java

---
+++ src/com/avp/ocp/Nurse.java
@@ -0,0 +1,21 @@
+package com.avp.ocp;
+
+public class Nurse extends Employee {
+
+    public Nurse(long id, String name, String department, boolean working) {
+        super(id, name, department, working);
+    }
+
+    private void checkVitalSigns(){
+        System.out.println("Checking Vitals");
+    }
+
+    private void drawBlood(){
+        System.out.println("drawing blood");
+    }
+
+    private void cleanPatientArea(){
+        System.out.println("cleaning Patient Area");
+    }
+
+}
