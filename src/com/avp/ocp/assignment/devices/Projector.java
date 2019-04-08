70e45c4708c7cc61336528e5349f338e8a8514e3
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2019-04-08 13:11:42 +0700

    OCP Principle: add Projector.java

---
+++ src/com/avp/ocp/assignment/devices/Projector.java
@@ -0,0 +1,23 @@
+package com.avp.ocp.assignment.devices;
+
+public class Projector implements Device {
+    @Override
+    public void turnOff() {
+        pullUpProjectorScreen();
+        System.out.println("Projector has been turned off");
+    }
+
+    @Override
+    public void turnOn() {
+        pullDownProjectorScreen();
+        System.out.println("Projector has been turned on");
+    }
+
+    private void pullDownProjectorScreen(){
+        System.out.println("Screen has been lowered for viewing");
+    }
+
+    private void pullUpProjectorScreen(){
+        System.out.println("Screen raise back up to close");
+    }
+}
