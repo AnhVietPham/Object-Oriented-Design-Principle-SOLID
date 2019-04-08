26aa4be9074add96ada0f1f2c07eca9c40d8ea63
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2019-04-08 15:40:17 +0700

    OCP Principle: Add RemoteControl.java

---
+++ src/com/avp/ocp/assignment/remotes/RemoteControl.java
@@ -0,0 +1,32 @@
+package com.avp.ocp.assignment.remotes;
+
+import com.avp.ocp.assignment.devices.Device;
+
+public class RemoteControl {
+    private static RemoteControl instance;
+    private Device device;
+
+    private RemoteControl() {
+
+    }
+
+    public static RemoteControl getInstance() {
+        if (instance == null) {
+            instance = new RemoteControl();
+        }
+        return instance;
+    }
+
+    public void connectToDevice(Device device) {
+        this.device = device;
+        System.out.println("---- connected to: "+ this.device + "----");
+    }
+
+    public void turnOnDevice() {
+        device.turnOn();
+    }
+
+    public void turnOffDevice() {
+        device.turnOff();
+    }
+}
